d5e3df671ff59af178bafff54f56d115cc4ac64f
PedroAntunes7 <102548027+PedroAntunes7@users.noreply.github.com>
2022-03-28 17:24:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My First Project
+          Alterei No Git
         </p>
         <a
           className="App-link"
