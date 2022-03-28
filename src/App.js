8ec1083f11b97e36cf5de14289ee5e19be277d64
PEDRO <cg3016455@LAB117M15.IFSP>
2022-03-28 17:25:54 -0300

    Comentei no VSC

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alterei No Git
+          Alterei No VSC
         </p>
         <a
           className="App-link"
