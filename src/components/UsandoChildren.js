4e092dc092cdb25082647c159355919a6979934a
PEDRO <cg3016455@LAB117M15.IFSP>
2022-06-10 17:38:33 -0300

    ENTITULANDO PAGES

--- src/components/UsandoChildren.js
+++ src/components/UsandoChildren.js
@@ -1,9 +1,14 @@
 const UsandoChildren = ({value, children}) => {  
     return (
+        <>
+        <div className="d-flex justify-content-center my-3">
+           <h1 className='title'>Praias</h1> 
+         </div>
         <div style={{background: "#345345"}}>
             {children}
             <p>Valor = {value}</p>
         </div>
+        </>
     );
   }
   
--- src/components/UsandoIcons.js
+++ src/components/UsandoIcons.js
@@ -4,6 +4,9 @@ import { HiOutlineTrash } from "react-icons/hi";
 const UsandoIcons = ({valor, children}) => {  
     return (
         <>
+        <div className="d-flex justify-content-center my-3">
+           <h1 className='title'>Contatos</h1> 
+        </div>
             <FaApple style={{
                 color: "#000",
                 width: '100px',
